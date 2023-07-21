2d6185186c8e0b0566f41dca6348035ec6adf2fd
Dane Smith <daneswildlife@gmail.com>
2023-07-21 15:24:46 -0600

    Dane's Friday Changes: Timer is working and ends with a popup window. Timer stops if the popup input window is closed. Attempted to work around this with a background script, but was unsuccessful.

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,5 @@
+chrome.runtime.onStartup.addListener(function() {
+
 const button = document.querySelector("button")
 
 // function timeOut() {
@@ -8,8 +10,10 @@ button.addEventListener('click', () => {
     setTimeout(() => alert(`Time is up! Go to bed!`), `${Number(document.querySelector("textarea").value)}` * 1000)
 });
 
-// button.addEventListener('click', () => {
-//     window.close(), 1000;
-// });
+button.addEventListener('click', () => {
+    window.close(), 1000;
+});
+
+})
 
 
