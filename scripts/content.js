97d4f56cef8757011a38610da9b32150e5ae3a56
Dane Smith <daneswildlife@gmail.com>
2023-07-21 15:37:07 -0600

    Minor Friday update with added comments on content page

--- scripts/content.js
+++ scripts/content.js
@@ -4,10 +4,14 @@ const button = document.querySelector("button")
 //     setTimeout(() => alert(`Time is up! Go to bed!`), `${Number(document.querySelector("textarea").value)}` * 1000);
 // }
 
+
+// listen for button click and setTimout to input, alert user when time is up
 button.addEventListener('click', () => {
     setTimeout(() => alert(`Time is up! Go to bed!`), `${Number(document.querySelector("textarea").value)}` * 1000)
 });
 
+
+// // listen for button click and close popup when clicked
 // button.addEventListener('click', () => {
 //     window.close(), 1000;
 // });
